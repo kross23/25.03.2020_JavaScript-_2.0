67f7d3c62bb2dea60da6d78bf82004d640e5bd7a
kross <662242@mail.ru>
2020-03-26 15:35:11 +0500

    первый комит

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let  money=0, 
+    income = '', 
+    addExpenses = '', 
+    deposit = 0, 
+    mission = 1, 
+    period = 0;
+    alert('модальном окне (alert) ');
+    console.log('Вывести в консоль сообщение с любым текстом');

10b8079269dcee30f9c64a03b139d7d5aa43ab88
kross <662242@mail.ru>
2020-03-28 00:19:32 +0500

    исправил lesson02

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-let  money=0, //переменныйе для задания
+let  money=20000, //переменныйе для задания
     income = '', 
-    addExpenses = '', 
+    addExpenses = 'автоэлектрик в автосервисе, электрик обычный, работа на севере', 
     deposit = 0, 
-    mission = 1, 
-    period = 0;
-    alert('модальное окно-(alert!) ');
+    mission = 1000000, 
+    period = 6,
+    budgetDay = Math.floor(money/30);
+  let  arr = addExpenses.split(',');
+    alert('модальное окно-(alert!)');
     console.log(' сообщение для консоли!');
+    console.log(typeof(money));
+    console.log(typeof(income));
+    console.log(typeof(deposit));
+    console.log('длина строки',addExpenses.length);
+    console.log('Период равен',period,'месяцев');
+    console.log('Цель заработать',mission ,'рублей/долларов/гривен/юани');
+    let a = addExpenses.toLocaleLowerCase();
+    console.log(a);
+    let b = addExpenses.toLocaleUpperCase();
+    console.log(b);
+    console.log( 'budgetDay', budgetDay);
+    console.log(arr,' строка разбита по запятым ');
+    arr = addExpenses.split('');
+    console.log(arr,' строка разбита по символам ');
